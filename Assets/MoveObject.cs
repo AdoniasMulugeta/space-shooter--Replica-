4e5d8cdff3fff0862bd784f2cfd8e57b3834ad4f
adonias <adonimm@gmail>
2018-01-25 22:33:54 +0300

    created and added the MoveObject on the Bolt Prefab object

---
+++ Assets/MoveObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObject : MonoBehaviour {
+	public float moveSpeed;
+	void Start () {
+		GetComponent<Rigidbody>().velocity = Vector3.forward * moveSpeed;
+	}
+}
