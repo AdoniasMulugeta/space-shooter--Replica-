da6c7c842f75ce63c8a0c87676f05343c3186369
adonias <adonimm@gmail>
2018-01-25 21:01:14 +0300

    added playermanager script along with the movement functionality using the keyboard

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent (typeof(Rigidbody))]
+public class PlayerManager : MonoBehaviour {
+	public float moveSpeed;
+
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		movePlayer();
+	}
+	void movePlayer(){
+		float xInput = Input.GetAxis("Horizontal") * moveSpeed;
+		float yInput = Input.GetAxis("Vertical") * moveSpeed;
+		rb.velocity = Vector3.right * xInput + Vector3.forward * yInput;
+	}
+}
