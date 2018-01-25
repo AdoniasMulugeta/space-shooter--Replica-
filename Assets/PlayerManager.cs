aa5347f445eba181ce441a6bdde72b37672c9d00
adonias <adonimm@gmail>
2018-01-25 21:07:00 +0300

    added the tiltPlayer() Method

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 [RequireComponent (typeof(Rigidbody))]
 public class PlayerManager : MonoBehaviour {
 	public float moveSpeed;
+	public float tiltAmount;
 
 	private Rigidbody rb;
 
@@ -12,10 +13,15 @@ public class PlayerManager : MonoBehaviour {
 
 	void Update () {
 		movePlayer();
+		tiltPlayer();
 	}
 	void movePlayer(){
 		float xInput = Input.GetAxis("Horizontal") * moveSpeed;
 		float yInput = Input.GetAxis("Vertical") * moveSpeed;
 		rb.velocity = Vector3.right * xInput + Vector3.forward * yInput;
+	}
+	void tiltPlayer(){
+		float xInput = Input.GetAxis("Horizontal") * -tiltAmount;
+		transform.rotation = Quaternion.Euler(Vector3.forward * xInput);
 	}
 }
