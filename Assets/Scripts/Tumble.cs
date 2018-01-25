1ba661882d784f011dcbb33634e4047e1f05277f
adonias <adonimm@gmail>
2018-01-26 00:15:28 +0300

    Added the Tumble Script which controls the random rotation of the obstacles generated when they are instantiated.

---
+++ Assets/Scripts/Tumble.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tumble : MonoBehaviour {
+	public float tumbleSpeed;
+	void Start () {
+		GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumbleSpeed;
+	}
+}
