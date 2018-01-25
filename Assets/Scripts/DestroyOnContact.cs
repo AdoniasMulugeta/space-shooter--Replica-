3818d17bc29b4d1243d033349ffa79d9463a57af
adonias <adonimm@gmail>
2018-01-26 00:56:51 +0300

    created DestroyOnContact Scripts to be used on objects with trigger colliders.

---
+++ Assets/Scripts/DestroyOnContact.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnContact : MonoBehaviour {
+	public GameObject deathParticle;
+	void OnTriggerEnter(Collider other){
+		if(deathParticle != null)
+			Instantiate(deathParticle,transform.position,transform.rotation);
+		Destroy(gameObject);
+	}
+}
