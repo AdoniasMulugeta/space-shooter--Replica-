2df24f89778975760960f515c28ac9f9c4d298b3
adonias <adonimm@gmail>
2018-01-25 22:53:56 +0300

    Moved all the scripts to the Scripts Folder and added the checkBoundary() Method into PlayerManager Script.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,5 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+
+[System.Serializable]
+public class Boundary{
+	public float left,right,top,bottom;
+}
 [RequireComponent (typeof(Rigidbody))]
 public class PlayerManager : MonoBehaviour {
 	public float moveSpeed;
@@ -7,6 +12,7 @@ public class PlayerManager : MonoBehaviour {
 	public GameObject boltPrefab;
 	public Transform boltSpawn;
 	public float refireWait;
+	public Boundary boundary;
 
 	private Rigidbody rb;
 	private float nextFire;
@@ -24,6 +30,7 @@ public class PlayerManager : MonoBehaviour {
 		float xInput = Input.GetAxis("Horizontal") * moveSpeed;
 		float yInput = Input.GetAxis("Vertical") * moveSpeed;
 		rb.velocity = Vector3.right * xInput + Vector3.forward * yInput;
+		checkBoundary();
 	}
 	void tiltPlayer(){
 		float xInput = Input.GetAxis("Horizontal") * -tiltAmount;
@@ -34,6 +41,11 @@ public class PlayerManager : MonoBehaviour {
 			Instantiate(boltPrefab,boltSpawn.position,boltSpawn.rotation);
 			nextFire = Time.time + refireWait;
 		}
-
+	}
+	void checkBoundary(){
+		float xClamp = Mathf.Clamp(transform.position.x,boundary.right,boundary.left);
+		float zClamp = Mathf.Clamp(transform.position.z,boundary.bottom,boundary.top);
+		float yClamp = transform.position.y;
+		transform.position = Vector3.right * xClamp + Vector3.up * yClamp + Vector3.forward * zClamp;
 	}
 }
