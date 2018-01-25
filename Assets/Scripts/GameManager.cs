35522105f20c1c1e7bfb1e9b6421d4f0c8d9ef24
adonias <adonimm@gmail>
2018-01-26 00:13:18 +0300

    added the spawnwave(),generateSpawnPoint() and checkBoundary() Methods which all control the generation and spawning of obstacles in a random position.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,32 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-	
+	public Boundary spawnBoundary;
+	public Transform spawnPointReference;
+	public GameObject[] astroids;
+	public float spawnWait;
+	public float waveWait;
 	void Start () {
-	
+		StartCoroutine(spawnObstacleWave(10,astroids,astroids.Length));
 	}
 
 	void Update () {
-		
+
+	}
+	Transform generateSpawnPoint(){
+		Transform spawnPoint = spawnPointReference;
+		spawnPoint.position = Vector3.up * spawnPoint.position.y +
+							  Vector3.forward * spawnPoint.position.z +
+							  Vector3.right * Random.Range(spawnBoundary.left,spawnBoundary.right);
+		return spawnPoint;
+	}
+	IEnumerator spawnObstacleWave(int obstacleAmount,GameObject[] obstacles,int variety){
+		for (int i=0; i < obstacleAmount; i++){
+			spawnObstacle(obstacles[Random.Range(0,Mathf.Clamp(variety,0,obstacles.Length))],generateSpawnPoint());
+			yield return new WaitForSeconds(spawnWait);
+		}
+	}
+	void spawnObstacle(GameObject obstacle,Transform spawnPoint){
+		Instantiate(obstacle,spawnPoint.position,spawnPoint.rotation);
 	}
 }
