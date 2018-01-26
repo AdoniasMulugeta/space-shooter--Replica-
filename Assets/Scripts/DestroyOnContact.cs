e78d7b969573c98ecc16506447172b2a87ae4d59
adonias <adonimm@gmail>
2018-01-26 23:42:14 +0300

    added events to broadcast colliisons b/n player, Asroids and Bolt player fires, so that we know when player destroy or get detroyed by astroids

--- Assets/Scripts/DestroyOnContact.cs
+++ Assets/Scripts/DestroyOnContact.cs
@@ -3,9 +3,20 @@ using System.Collections;
 
 public class DestroyOnContact : MonoBehaviour {
 	public GameObject deathParticle;
+	public delegate void destroyEvents();
+	public static event destroyEvents eventObstacleDestroyed;
+	public static event destroyEvents eventPlayerDestroyed;
+
 	void OnTriggerEnter(Collider other){
-		if(deathParticle != null)
+		if(deathParticle != null){
 			Instantiate(deathParticle,transform.position,transform.rotation);
+		}
+		if(gameObject.tag == "Bolt" && other.tag == "Obstacle"){
+			if (eventObstacleDestroyed != null) eventObstacleDestroyed();
+		}
+		if(gameObject.tag == "Player" && other.tag == "Obstacle"){
+			if (eventPlayerDestroyed != null) eventPlayerDestroyed();
+		}
 		Destroy(gameObject);
 	}
 }
